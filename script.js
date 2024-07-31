71d2dae4477aa4e96260bd09da0e5e0d5b124ec3
Gagan499 <157002027+Gagan499@users.noreply.github.com>
2024-07-31 14:00:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function popState() {
     let state = prompt("Enter the state");
     console.log(`You entered: ${state}`);
     if (state === 'Punjab' || state === 'Haryana' || state === 'Chandigarh') {
-        let userCredentials = User();
-        all(state, userCredentials.username, userCredentials.pass);
+        User();
     }
     else {
         alert("Acess Deined");
@@ -15,9 +14,19 @@ function User() {
     if (username === 'Punjab' || username === 'Haryana' || username === 'Chandigarh') {
         let pass = prompt("Enter the password : ");
         console.log(`Username entered : ${username}`);
-        if (pass === 'Punjab' || pass === 'Haryana' || pass === 'Chandigarh') {
+        if (pass === 'Punjab' || pass === 'Chandigarh') {
+            window.location.href = 'https://maps.app.goo.gl/kLc4nBPLYDbdXvoE9';
             console.log(`correct Username and Password entered : ${username} , ${pass}`);
             return { username, pass };
+        }
+        else if(pass === 'Haryana'){
+            console.log(`correct Username and Password entered : ${username} , ${pass}`);
+            window.location.href = 'https://maps.app.goo.gl/eFMGLo8VYKkj8UVa6';
+
+        }
+        else if(pass === 'Chandigarh'){
+            console.log(`correct Username and Password entered : ${username} , ${pass}`);
+            window.location.href = 'https://maps.app.goo.gl/ViE9yMwVJYBvfEge9';
         }
         else {
             alert(`Incoorect password enterd : ${pass}`)
@@ -27,17 +36,5 @@ function User() {
     else {
         alert(`Incorrect Username Entered  ${username}`)
         console.log(`Incorrect Username entered : ${username}`);
-    }
-}
-
-function all(state, username, pass) {
-    if (state === 'Punjab' || username === 'Punjab' || pass === 'Punjab') {
-        window.location.href = 'https://maps.app.goo.gl/kLc4nBPLYDbdXvoE9';
-    }
-    else if (state === 'Haryana') {
-        window.location.href = 'https://maps.app.goo.gl/eFMGLo8VYKkj8UVa6';
-    }
-    else if (state === 'Chandigarh') {
-        window.location.href = 'https://maps.app.goo.gl/ViE9yMwVJYBvfEge9';
     }
 }
